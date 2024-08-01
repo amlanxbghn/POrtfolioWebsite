eb236cdfe1fe1552360a4873a3340e0585de61b6
amlanxbghn <amlanxconnect@gmail.com>
2024-08-02 04:40:36 +0530

    minor changes

--- src/components/LandingPage/Marquee.jsx
+++ src/components/LandingPage/Marquee.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 
 function Marquee() {
   return (
-    <div className='w-full py-20 rounded-tl-3xl bg-[#2185D5]'>
+    <div className='w-full py-20 bg-[#FC4100]'>
       <div className="text border-t-2 border-b-2 border-zinc-300 flex overflow-hidden whitespace-nowrap">
         <motion.div 
           initial={{ x: "100%" }} 
           animate={{ x: "-100%" }} 
-          transition={{ repeat: Infinity, ease: "linear", duration: 10 }} 
+          transition={{ repeat: Infinity, ease: "linear", duration: 25}} 
           className='flex'>
-          <h1 className='text-[12vw] leading-none font-["Sans-Serif"] uppercase mt-10 mb-10 font-semibold'>Software Engineer</h1>
-          <h1 className='text-[12vw] leading-none font-["Sans-Serif"] uppercase mt-10 mb-10 font-semibold'>Software Engineer</h1>
-          <h1 className='text-[12vw] leading-none font-["Sans-Serif"] uppercase mt-10 mb-10 font-semibold'>Software Engineer</h1>
+          <h1 className='text-[12vw] leading-none font-["Inter"] uppercase mt-10 mb-10 font-semibold text-white'>Eat&nbsp;&#183;&nbsp;Sleep&nbsp;&#183;&nbsp;Code&nbsp;&#183;&nbsp;Repeat&nbsp;&#183;&nbsp;</h1>
+          <h1 className='text-[12vw] leading-none font-["Inter"] uppercase mt-10 mb-10 font-semibold text-white'>Eat&nbsp;&#183;&nbsp;Sleep&nbsp;&#183;&nbsp;Code&nbsp;&#183;&nbsp;Repeat&nbsp;&#183;&nbsp;</h1>
+          <h1 className='text-[12vw] leading-none font-["Inter"] uppercase mt-10 mb-10 font-semibold text-white'>Eat&nbsp;&#183;&nbsp;Sleep&nbsp;&#183;&nbsp;Code&nbsp;&#183;&nbsp;Repeat&nbsp;&#183;&nbsp;</h1>
         </motion.div>
       </div>
     </div>
