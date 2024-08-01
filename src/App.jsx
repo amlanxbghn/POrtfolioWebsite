2399b8a976e2e438fa9c79f1fc84777e4cacc30b
amlanxbghn <amlanxconnect@gmail.com>
2024-08-02 01:41:48 +0530

    Landing Pag

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import LandingPage from './components/LandingPage'
-import Marquee from './components/Marquee'
-import About from './components/About'
+import LandingPage from './components/LandingPage/LandingPage'
+import Marquee from './components/LandingPage/Marquee'
+import About from './components/About/About'
 import LocomotiveScroll from 'locomotive-scroll';
 
 function App() {
   const locomotiveScroll = new LocomotiveScroll();
   return (
-    <div className='w-full min-h-screen text-white bg-black'>
+    <div className='w-full min-h-screen'>
       <Navbar />
       <LandingPage />
       <Marquee />
---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div className='landing-page'>
+      <h1 className='text-4xl text-[#767676]'>
+        <span className="emoji">&#128075;&nbsp;</span>
+        Howdy, My name is
+      </h1>
+      <h1 className='text-9xl'>Amlan BG</h1>
+      <h1 className='text-4xl text-[#767676]'>Software Developer with a keen Interest in Design</h1>
+      <h1 className='text-4xl text-[#767676]'>Currently Based out of Bengaluru, IN</h1>
+      <div className="divider"></div>
+    </div>
+  );
+}
+
+export default LandingPage;
